74bbc285b80a75ad82e5ea1bf4a828b29cf6ee8b
Sinhran <78912276+Sinhran@users.noreply.github.com>
2024-12-13 07:07:26 +0530

    One Gold Star Closer

--- 2024/day1/day1.ts
+++ 2024/day1/day1.ts
@@ -1,23 +1,38 @@
 const bytes: string = await Deno.readTextFile("list.txt");
-let pairs: string[] = bytes.trim().split("   ");
-// pairs = pairs.split("    ");
+let pairs: string[] = bytes.trim().split("\n");
+
 let arlen: number = pairs.length;
-// console.log(`${pairs}`);
 
-let pairs_left = new Array(Math.floor(arlen / 2));
-let pairs_right = new Array(Math.floor(arlen / 2));
+let broken: string[][] = pairs.map(line => line.split("   "));
 
-let pointer: numebr = 0;
-while (pointer < arlen) {
-  if (pointer%2 == 0) {
-    pairs_left[pointer] = pairs[pointer];
-  } else {
-    pairs_right[pointer] = pairs[pointer];
-  }
-  pointer++;
+let pairs_left = new Array(arlen);
+let pairs_right = new Array(arlen);
+
+for (let i = 0; i < arlen; i++) {
+  pairs_left[i] = Number(broken[i][0]);
+  pairs_right[i] = Number(broken[i][1]);
 }
 
+for (let i = 0; i < arlen; i++) {
+  let temp: number = 0;
+  for (let j = 0; j < (arlen - i - 1); j++) {
+    if (pairs_left[j] > pairs_left[j + 1]) {
+      temp = pairs_left[j];
+      pairs_left[j] = pairs_left[j + 1];
+      pairs_left[j + 1] = temp;
+    }
+    if (pairs_right[j] > pairs_right[j + 1]) {
+      temp = pairs_right[j];
+      pairs_right[j] = pairs_right[j + 1];
+      pairs_right[j + 1] = temp;
+    }
+  }
+}
 
-console.log(`${pairs_left[1]}`);
-// console.log(`${pairs_right[1]}`);
-console.log(`${arlen}`);
+let distance: number = 0;
+
+for (let i = 0; i < arlen; i ++) {
+  distance = distance + Math.abs(pairs_left[i] - pairs_right[i]);
+  console.log(pairs_left[i] - pairs_right[i]);
+}
+console.log(distance);
