734a385ca51796cdddef8aa5a6a7f30b7f7077b4
Sinhran <78912276+Sinhran@users.noreply.github.com>
2025-01-02 18:43:46 +0530

    Day2 finished

--- 2024/day2/day2.ts
+++ 2024/day2/day2.ts
@@ -1,32 +1,58 @@
 const bytes: string = await Deno.readTextFile("levels.txt");
-let levels: string[] = bytes.trim().split("\n");
+let lines: string[] = bytes.trim().split("\n");
+const arlen: number = lines.length;
+let levels: number[][] = [];
 
-const arlen: number = levels.length;
+for(let i = 0; i < arlen; i++) {
+  levels.push(lines[i].split(" "));
+  for(let j = 0; j < levels[i].length; j++) {
+    levels[i][j] = Number(levels[i][j]);
+  }
+}
 
-let llvs: number[][] = new Array(arlen).map(() => []);
-for (let i = 0; i < arlen; i++) {
-  llvs[i] = levels[i].split(" ").map(Number);
+function diff(arr: number[]): number[]{
+  let k = arr.length - 1;
+  let temp = new Array<number>(k);
+  for(let i = 0; i < k; i++) {
+    temp[i] = arr[i + 1] - arr[i];
+  }
+  return temp;
 }
 
 let safeRep: number = 0;
-let badRep: number = 0;
-let deff: number[][] = [];
-let diff: number[][] = [];
-const allowedVal = new Set([1, 2, 3]);
-for (let i = 0; i < arlen; i++) {
-  const llen: number = llvs[i].length;
-  let lineDeff: number[] = [];
-  for (let j = 0; j < llen - 1; j++) {
-    lineDeff.push(Math.abs(llvs[i][j] - llvs[i][j + 1]));
-  }
-  deff.push(lineDeff);
+for(let i = 0; i < arlen; i++) {
+  let increasing: boolean = true;
+  let decreasing: boolean = true;
 
-  let lineDiff: number[] = [];
-  for (let j = 0; j < arlen - 1; j++) {
-    lineDiff.push(deff[i][j] - deff[i][j + 1]);
+  let differences = diff(levels[i]);
+
+  for(let j = 0; j < differences.length; j++) {
+    if(differences[j] > 0) {
+      decreasing = false;
+    } else if(differences[j] < 0) {
+      increasing = false;
+    } else {
+      increasing = false;
+      decreasing = false;
+    }
   }
-  diff.push(lineDiff);
+
+  let vD: boolean = true;
+  for(let j = 0; j < differences.length; j++) {
+    if(increasing) {
+      if(differences[j] < 1 || differences[j] > 3) {
+        vD= false;
+      }
+    } else if(decreasing) {
+      if(differences[j] > -1 || differences[j] < -3) {
+        vD = false;
+      }
+    } else {
+      vD = false;
+    }
+  }
+  if(vD && (increasing || decreasing)) {
+    safeRep++;
+  }
 }
-console.log(diff);
-// console.log(safeRep);
-// console.log(badRep);
+  console.log(safeRep);
